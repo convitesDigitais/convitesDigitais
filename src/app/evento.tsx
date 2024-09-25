32c0d7c1379d3e841b986633b2d4a07261e030ad
dvmicrocredito <dvmicrocredito@gmail.com>
2024-09-25 08:00:33 +0200

    reparos10

--- src/app/evento.tsx
+++ src/app/evento.tsx
@@ -1,26 +1,36 @@
-"use server";
-
 export const getUser = async () => {
-  const data = await fetch("https://www.gestaoconvites.com/api/obterUser");
+  const data = await fetch("https://www.gestaoconvites.com/api/obterUser", {
+    cache: "no-store",
+  });
   return data.json();
 };
 export const getEventos = async () => {
-  const data = await fetch("https://www.gestaoconvites.com/api/obterEvento");
+  const data = await fetch("https://www.gestaoconvites.com/api/obterEvento", {
+    cache: "no-store",
+  });
   return data.json();
 };
 export const getConvidados = async () => {
   const data = await fetch(
-    "https://www.gestaoconvites.com/api/obterConvidados"
+    "https://www.gestaoconvites.com/api/obterConvidados",
+    {
+      cache: "no-store",
+    }
   );
   return data.json();
 };
 export const getCategorias = async () => {
   const data = await fetch(
-    "https://www.gestaoconvites.com/api/obterCategorias"
+    "https://www.gestaoconvites.com/api/obterCategorias",
+    {
+      cache: "no-store",
+    }
   );
   return data.json();
 };
 export const getMesas = async () => {
-  const data = await fetch("https://www.gestaoconvites.com/api/obterMesas");
+  const data = await fetch("https://www.gestaoconvites.com/api/obterMesas", {
+    cache: "no-store",
+  });
   return data.json();
 };
